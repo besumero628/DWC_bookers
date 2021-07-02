3926b8109920050952e7a91b2f36d5af15ca2e5b
besumero628 <besumero628@gmail.com>
2021-07-02 15:11:25 +0000

    [Update] asset stylesheet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
+  
+  private
+  def set_layout
+    if params[:controller].match(%r{\A(homes|books)})
+      Regexp.last_match[1]
+    else
+      "books"
+    end
+  end
 end
