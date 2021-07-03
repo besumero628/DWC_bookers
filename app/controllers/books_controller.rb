f43fa7e68c252e67ed5dc6d9a8043e18cbfff298
besumero628 <besumero628@gmail.com>
2021-07-03 14:58:19 +0000

    [Update] show,edit page & validation error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,13 +3,39 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def create
     book = Book.new(book_params)
     book.save
     redirect_to books_path
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash.notice = "Book was successfully updated."
+      redirect_to book_path(params[:id])
+    else
+      render "books/edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash.notice = "Book was successfully deleted."
+    end
+    redirect_to books_path
+  end
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
