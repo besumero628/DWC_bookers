c4ee61d8db60f80fdd03b233f3800b86f6503032
besumero628 <besumero628@gmail.com>
2021-07-03 03:07:12 +0000

    [Update] books/index page layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,14 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
